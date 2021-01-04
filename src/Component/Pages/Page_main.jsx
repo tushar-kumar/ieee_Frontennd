78b27080779aa0a25df5bd2ee3928b5f6a2aa7e6
Tushar Kumar <65044832+tushar-kumar@users.noreply.github.com>
2021-01-04 22:39:59 +0530

    Update Page_main.jsx

--- src/Component/Pages/Page_main.jsx
+++ src/Component/Pages/Page_main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Page_main() {
     return (
         <div>
-            <div>page</div>
+            <div>Main Page</div>
         </div>
     );
 }
